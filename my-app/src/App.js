bdf7433d3d4bf89b37a09ac9c530e9b43f966a96
Dillon Gorlesky <dmgorlesky1@catamount.wcu.edu>
2021-04-12 22:56:44 -0400

    Stuff shows

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,56 +10,75 @@ import { csv } from 'd3';
 
 
 function App() {
-    const height = 500;
-    const width = 500;
-    const nodes = new Request('Data/nodesInfo.json');
+    const height = 9000;
+    const width = 9000;
+    //const nodes = new Request('Data/nodesInfo.json');
+    const nodes = new Request('Data/scratch.json');
+
     const svgRef = useRef();
     useEffect(() => {
         fetch(nodes).then(response => response.json()).then(data => {
         //loops below just display contents of our nodesInfo.json file in the browser's console
         //it takes FOREVER to log these
-        /** for (const obj of data['nodes']){
-            console.log("Name: " + obj['name']);
-        }
 
-         for (const obj of data['links']){
-            console.log("source: " + obj['source']);
-            console.log("target: " + obj['target']);
-        }*/
 
          //create somewhere to put the force directed graph
-         let svg = d3.select(svgRef.current),
-         width = +svg.attr("width"),
-         height = +svg.attr("height");
+         let svg = d3.select(svgRef.current)
+             .append("svg")
+             .attr("preserveAspectRatio", "xMinYMin meet")
+             .attr("viewBox", "0 0 300 300")
+             .classed("svg-content", true);
 
 
-         const nodes_data = data['nodes'];
+            const nodes_data = data['nodes'];
+
+            //Create links data
+            const links_data = data['links'];
+
+            let link_force = d3.forceLink(links_data)
+                .id(function (d) {
+                    return d.name;
+                })
+
+            let simulation = d3.forceSimulation()
+                .nodes(nodes_data);
+
+            simulation
+                .force("charge_force", d3.forceManyBody())
+                .force("center_force", d3.forceCenter(width / 2, height / 2));
 
 
-         //set up the simulation
-         //nodes only for now
-         let simulation = d3.forceSimulation()
-         //add nodes
-         .nodes(nodes_data);
+            let link = svg.append("g")
+                .attr("class", "links")
+                .selectAll("line")
+                .data(links_data)
+                .enter().append("line")
+                .attr("stroke-width", 2);
 
-         //add forces
-         //we're going to add a charge to each node
-         //also going to add a centering force
-         simulation
-         .force("charge_force", d3.forceManyBody())
-         .force("center_force", d3.forceCenter(width / 2, height / 2));
 
+            let text = svg.append("g")
+                .attr("class", "nodes")
+                .selectAll("text")
+                .data(nodes_data)
+                .enter()
+                .append("text")
+                .attr("x", function(d) { return d.x })
+                .attr("y", function(d) { return d.y })
+                .text(function(d) { return d.name });
 
          //draw circles for the nodes
-         let node = svg.append("g")
-         .attr("class", "nodes")
-         .selectAll("circle")
-         .data(nodes_data)
-         .enter()
-         .append("circle")
-         .attr("r", 5)
-         .attr("fill", "red");
+            let node = svg.append("g")
+                .attr("class", "nodes")
+                .selectAll("circle")
+                .data(nodes_data)
+                .enter()
+                .append("circle")
+                .attr("r", 5)
+                .attr("fill", "red");
 
+
+            node.append("text")
+                node.text(function (d) { return d.name });
 
          //add tick instructions:
          simulation.on("tick", tickActions);
@@ -67,31 +86,11 @@ function App() {
 
          //Time for the links
 
-         //Create links data
-         const links_data = data['links'];
-
-         //Create the link force
-         //We need the id accessor to use named sources and targets
-
-         let link_force = d3.forceLink(links_data)
-         .id(function (d) {
-             return d.name;
-         })
-
          //Add a links force to the simulation
          //Specify links  in d3.forceLink argument
 
 
          simulation.force("links", link_force)
-
-         //draw lines for the links
-         let link = svg.append("g")
-            .attr("class", "links")
-            .selectAll("line")
-            .data(links_data)
-            .enter().append("line")
-            .attr("stroke-width", 2);
-
 
          function tickActions() {
                     //update circle positions each tick of the simulation
