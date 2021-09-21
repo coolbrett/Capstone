b9377b63b95a42b9b8afb9a02045261032f7f191
Dillon Gorlesky <dmgorlesky1@catamount.wcu.edu>
2021-09-20 22:27:52 -0400

    Query Button on Navbar now shows a list of 25 movie names in the console of the GUI

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = () => {
                 </h3>
                <ul className={click ?  'nav-menu active' : 'nav-menu'}>
                 <FindNode />
-                   <QueryButton></QueryButton>
+                   <QueryButton />
                </ul>
                <p style={{color:"white"}}>Listed: </p>
                <p style={{color:"white"}}>{nodeId}</p>
--- my-app/src/components/QueryButton.js
+++ my-app/src/components/QueryButton.js
@@ -1,21 +1,53 @@
 import React, { Component, useState } from 'react';
 import './Button.css';
-import {useReadCypher} from "use-neo4j";
+import {useReadCypher, useReadTransaction} from "use-neo4j";
 
 export function QueryButton() {
 
-    const query = 'MATCH (n: Movie) RETURN n LIMIT 25'
+    const query = `MATCH (n: Movie) RETURN n LIMIT 25`
+    const params = {title: 'Guardians of the Galaxy'}
 
-    const resultState = useReadCypher(query, '', 'MovieData')
+    const { resultState, first} = useReadCypher('MovieData');
 
+    //console.log(resultState + " Result State");
+    //console.log(first + " first");
     // Get `m` from the first row
     //const movie = first.get('m');
-    console.log("in QueryButton");
+   /** console.log("in QueryButton");
     console.log("Query returns: " + resultState)
     console.log("Still Loading? : " + resultState.loading);
-    console.log(resultState)
+    console.log(resultState)*/
+
+    const changing = (async() => {
+       const neo4j = require('neo4j-driver')
+
+       const uri = 'neo4j+s://4f877cd8.databases.neo4j.io';
+       const user = 'neo4j';
+       const password = '1TIT1myoa1kmE-TkrEmeZab6GvLzax8DTif-SW4HFK8';
+
+       const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+       const session = driver.session()
+
+       try{
+           const readQuery = `MATCH (n: Movie) RETURN n LIMIT 25`
+           const readResult = await session.readTransaction(tx =>
+               tx.run(readQuery, {})
+           )
+           readResult.records.forEach(record => {
+               //.get('n')
+               console.log(`Found movie: ${record.get('n')}`)
+           })
+       } catch (error){
+           console.error('Something went wrong: ', error)
+       } finally {
+           await session.close();
+       }
+
+    })
 
     return (
-        <div></div>
+        <div>
+            <button type={"submit"} onClick={changing}>Query</button>
+        </div>
     )
 }
